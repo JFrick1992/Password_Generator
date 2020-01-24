9d940b3279e14be4483ea734ead6f66532ca5f0d
Jacob Frick <jacobhfrick1992@gmail.com>
2020-01-23 20:58:17 -0800

    Added password length range slider

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -1,1 +1,5 @@
-
+function updateSliderCount() {
+  document.getElementById("passLenCount").innerHTML = document.getElementById("formControlRange").value
+}
+document.getElementById("passLenCount").innerHTML = "8"
+document.getElementById("formControlRange").addEventListener("input", updateSliderCount)

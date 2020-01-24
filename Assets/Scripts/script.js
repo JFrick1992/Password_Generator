26b1d12094d8f0af1f113300f720e1b38bb700b5
Jacob Frick <jacobhfrick1992@gmail.com>
2020-01-24 10:48:06 -0800

    Modal dismisses via button conntected to JS

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -1,5 +1,9 @@
 function updateSliderCount() {
   document.getElementById("passLenCount").innerHTML = document.getElementById("formControlRange").value
 }
+document.getElementById("generateBtn").addEventListener("click", function() {
+  console.log("hello")
+  $('#exampleModal').modal('hide')
+})
 document.getElementById("passLenCount").innerHTML = "8"
 document.getElementById("formControlRange").addEventListener("input", updateSliderCount)
